9ba8d4132a074c92309943d54bbb12c675192280
Robert Smith <rbrtsmith84@googlemail.com>
2016-06-20 12:16:17 +0100

    Add SVG build task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,6 +17,9 @@ import autoprefixer from 'autoprefixer';
 import cleanCss from 'gulp-clean-css';
 import sourcemaps from 'gulp-sourcemaps';
 import sequence from 'run-sequence';
+import svgstore from 'gulp-svgstore';
+import svgmin from 'gulp-svgmin';
+import path from 'path';
 import browserSync from 'browser-sync';
 browserSync.create();
 
@@ -26,7 +29,9 @@ const Paths = {
   JS_ENTRY: './src/app.js',
   HTML_SRC: 'src/index.html',
   SASS_SRC: './src/scss/**/*.scss',
-  SASS_OUT: './dist/assets/css'
+  SASS_OUT: './dist/assets/css',
+  SVG_SRC:  './src/assets/images/icons/*.svg',
+  SVG_OUT:  './dist/assets/images/'
 };
 
 const release = argv.release || false;
@@ -70,10 +75,6 @@ gulp.task('build:js', () => {
 
     watcher.on('log', gutil.log);
 
-    browserSync.init({
-      server: Paths.OUT,
-      logFileChanges: false
-    });
   } else {
     bundle(browserify(Paths.JS_ENTRY));
   }
@@ -91,9 +92,14 @@ gulp.task('sass', () => {
       remove: false
     })
   ];
+
+  const includePaths = [
+    './node_modules/sass-foundation/scss/'
+  ];
+
   return gulp.src(Paths.SASS_SRC)
     .pipe(gulpif(Flags.DEV, sourcemaps.init()))
-    .pipe(sass().on('error', gutil.log))
+    .pipe(sass({ includePaths }).on('error', gutil.log))
     .pipe(postCss(postCssProcessors))
     .pipe(gulpif(Flags.DEV, sourcemaps.write()))
     .pipe(gulpif(Flags.RELEASE, cleanCss({ advanced: false })))
@@ -122,14 +128,47 @@ gulp.task('lint:scss', () => {
 gulp.task('copy:html', () => {
   return gulp
     .src(Paths.HTML_SRC)
-    .pipe(gulp.dest(Paths.OUT));
+    .pipe(gulp.dest(Paths.OUT))
+    .pipe(gulpif(Flags.WATCH, browserSync.stream()));
 });
 
 
-gulp.task('bundle', ['copy:html', 'sass', 'build:js'], () => {
-  gulp.watch(Paths.SASS_SRC, ['sass']);
+gulp.task('create:svgSprite', () => {
+  return gulp.src(Paths.SVG_SRC)
+    .pipe(svgmin(file => {
+      const prefix = path.basename(file.relative, path.extname(file.relative));
+      return {
+        plugins: [{
+          cleanupIDs: {
+            prefix: `${prefix}-`,
+            minify: true
+          }
+        }]
+      };
+    }))
+    .pipe(svgstore())
+    .pipe(gulp.dest(Paths.SVG_OUT))
+    .pipe(gulpif(Flags.WATCH, browserSync.stream()));
 });
 
-gulp.task('build', () => sequence('clean', 'bundle'));
+gulp.task('browserSync:init', () => {
+  if (Flags.WATCH) {
+    browserSync.init({
+      server: Paths.OUT,
+      logFileChanges: false
+    });    
+  }
+});
+
+
+gulp.task('bundle', ['copy:html', 'create:svgSprite', 'sass', 'build:js'], () => {
+  if (Flags.WATCH) {
+    gulp.watch(Paths.HTML_SRC, ['copy:html']);
+    gulp.watch(Paths.SVG_SRC, ['create:svgSprite']);
+    gulp.watch(Paths.SASS_SRC, ['sass']);    
+  }
+});
+
+gulp.task('build', () => sequence('clean', 'browserSync:init', 'bundle'));
 
 gulp.task('default', ['build']);
