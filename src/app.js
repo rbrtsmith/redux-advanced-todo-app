82edd37b14a9a9c9e410275f0df289d884e26554
Robert Smith <rbrtsmith84@googlemail.com>
2016-06-20 13:12:50 +0100

    add svg4everybody to ensure external SVG sprite loads in IE

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import svg4everybody from 'svg4everybody';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
 
+
 ReactDOM.render(<Root />, document.querySelector('#app'));
+svg4everybody();
