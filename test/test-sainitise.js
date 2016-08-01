068a6f2b08fefc7a3239a7ca623ddab0187cab4b
Robert Smith <rbrtsmith84@googlemail.com>
2016-08-01 16:26:26 +0100

    update README

--- test/test-sainitise.js
+++ test/test-sainitise.js
@@ -4,8 +4,8 @@ import sanitise from '../src/utils/sanitise';
 
 test('It should return a lowercase string', t => {
 
-  const actual = 'hello world!';
-  const expected = sanitise('HELLO World!');
+  const actual = sanitise('HELLO World!');
+  const expected = 'hello world!';
 
   t.deepEqual(actual, expected);
 });
