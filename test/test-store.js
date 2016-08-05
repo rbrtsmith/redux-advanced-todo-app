63ac11a6c41531ed2b7ff68061c558d1280523f3
Robert Smith <rbrtsmith84@googlemail.com>
2016-08-05 23:36:47 +0100

    test store

--- test/test-store.js
+++ test/test-store.js
@@ -2,16 +2,23 @@ import test from 'ava';
 import proxyquire from 'proxyquire';
 import sinon from 'sinon';
 import { jsdom } from 'jsdom';
+import { createStore } from 'redux';
+
+import rootReducer from '../src/reducers/index';
+import todos from '../src/data/todos';
+import updateForms from '../src/data/updateForms';
+import addTodoFormFieldValues from '../src/data/addTodoFormFieldValues';
 
 global.document = jsdom('<body></body>');
 global.window = document.defaultView;
 global.navigator = window.navigator;
 
-test('It should call createStore passing in 3 arguments', t => {
+
+test('It should call createStore passing in rootReducer and defaultState', t => {
   const stubbedCreateStore = sinon.stub();
   const stubbedSyncHistoryWithStore = sinon.stub();
   const store = proxyquire('../src/store', {
-    'redux': {
+    redux: {
       createStore: stubbedCreateStore
     },
     'react-router-redux': {
@@ -19,7 +26,34 @@ test('It should call createStore passing in 3 arguments', t => {
     }
   }).default;
 
-  t.plan(2);
-  t.true(stubbedCreateStore.calledWith());
+  const defaultState = {
+    todos,
+    updateForms,
+    addTodoFormFieldValues
+  };
+
+  t.true(stubbedCreateStore.calledWith(rootReducer, defaultState));
+});
+
+
+test('It should call syncHistoryWithStore', t => {
+  const stubbedCreateStore = sinon.stub();
+  const stubbedSyncHistoryWithStore = sinon.stub();
+  const store = proxyquire('../src/store', {
+
+    'react-router-redux': {
+      syncHistoryWithStore: stubbedSyncHistoryWithStore
+    }
+  }).default;
+
+
+  const defaultState = {
+    todos,
+    updateForms,
+    addTodoFormFieldValues
+  };
+  
+  const argStore = createStore(rootReducer, defaultState);
+
   t.true(stubbedSyncHistoryWithStore.calledWith());
 });
