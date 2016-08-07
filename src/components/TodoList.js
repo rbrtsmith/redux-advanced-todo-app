7dbdb5c9b99fa2a0f4d7d08ab473d576a094bf1f
Robert Smith <rbrtsmith84@googlemail.com>
2016-08-07 09:33:46 +0100

    -

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import TodoCard from './TodoCard';
-
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 const TodoList = ({ todos }) => (
-  <ul className="o-bare-list o-bare-list--spaced">
-    {todos.map(todo => <TodoCard key={todo.id} {...todo} />)}
-  </ul>
+  <ReactCSSTransitionGroup
+    transitionName="example"
+    transitionEnterTimeout={300}
+    transitionLeaveTimeout={300}
+    component="ul"
+    className="o-bare-list o-bare-list--spaced"
+  >
+  {todos.map(todo => (
+    <TodoCard key={todo.id} {...todo} />
+  ))}
+  </ReactCSSTransitionGroup>
 );
 
 TodoList.propTypes = {
